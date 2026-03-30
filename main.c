97bd9d02bacf9e8556e020237279d513eabbf5b0
MasterVAE <voilov07@gmail.com>
2026-03-30 21:09:24 +0300

    added comments & codestyle

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 extern void _my_printf(const char* format, ...);
 
+
 int main()
 {
-    _my_printf("bebra %d \n %s %o\n", -10, "neahsgd", 10);
+    _my_printf("%o\n%d %s %x %d%%%c%b\n%d %s %x %d%%%c%b\n", -1, -1, "love", 3802, 100, 33, 127, -1, 
+                                                                        "love", 3802, 100, 33, 127);
 
     return 0;
 }
